7928596fe657946beb37e8e82158ac359b6193fd
ovan <geoyansen@gmail.com>
2026-06-06 18:50:29 +0700

    Clean up Pemulihan Data feature and unused test files

--- resources/views/koordinator/components/sidebar.blade.php
+++ resources/views/koordinator/components/sidebar.blade.php
@@ -175,10 +175,6 @@
             <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"/></svg>
             <span x-show="sidebarOpen" x-transition>Backup Database</span>
         </a>
-        <a href="{{ route('koordinator.pemulihan-data.index') }}" class="{{ $active == 'pemulihan-data' ? $subActiveClass : $subInactiveClass }}">
-            <svg class="w-4 h-4 flex-shrink-0" fill="none" stroke="currentColor" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 4v5h.582m15.356 2A8.001 8.001 0 004.582 9m0 0H9m11 11v-5h-.581m0 0a8.003 8.003 0 01-15.357-2m15.357 2H15"/></svg>
-            <span x-show="sidebarOpen" x-transition>Pemulihan Data</span>
-        </a>
     </div>
 </div>
 
